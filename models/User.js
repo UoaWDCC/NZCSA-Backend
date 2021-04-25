be59138a9fd68a7d4d6dba1cd0d52ff729f1acb0
MeloGuan <meloguan123@gmail.com>
2021-04-25 17:19:42 +1200

    completed resetpassword api Issue: User needs to reset the password when they forgot the password Solution: implement a api for resetting the password Risk: No Reviewed by: Melo, Garfield

--- models/User.js
+++ models/User.js
@@ -1,53 +1,64 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const cryto = require('crypto');
 const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: [true, "Please provide a username"]
-    },
-    email: {
-        type: String,
-        required: [true, "Please provide a email"],
-        unique: true,
-        match: [
-            /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/,
-            "Please provide a valid email"
-        ]
-    },
-    password: {
-        type: String,
-        required: [true, "Please provide a password"],
-        minlength: 6,
-        select: false
-    },
-    resetPasswordToken: String,
-    resetPasswordExpire: Date,
+  username: {
+    type: String,
+    required: [true, 'Please provide your first name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide a email'],
+    unique: true,
+    match: [
+      /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/,
+      'Please provide a valid email',
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 6,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
 });
 
-UserSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) {
-        next();
-    }
-
-    const salt = await bcrypt.genSalt(10);
-
-    this.password = await bcrypt.hash(this.password, salt);
-
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
     next();
+  }
 
+  const salt = await bcrypt.genSalt(10);
+
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
 });
 
 UserSchema.methods.matchPassword = async function (password) {
-    return await bcrypt.compare(password, this.password);
+  return await bcrypt.compare(password, this.password);
 };
 
 UserSchema.methods.getSignedToken = function () {
-    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE });
-}
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE });
+};
 
+UserSchema.methods.getResetPasswordToken = function () {
+  const resetToken = cryto.randomBytes(20).toString('hex');
+  console.log('Before:', resetToken);
+  this.resetPasswordToken = cryto.createHash('sha256').update(resetToken).digest('hex');
+  console.log('After: ', this.resetPasswordToken);
 
-const User = mongoose.model("User", UserSchema);
+  this.resetPasswordExpire = Date.now() + 10 * (60 * 1000);
+
+  return resetToken;
+};
+
+// Creating MongoDB model (Relation schema('User') in MongoDB atlas)
+const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,14 @@
-const { register, login, forgotpassword, resetpassword } = require('../controllers/auth');
-
-
 const express = require('express');
+const {
+  register, login, forgotpassword, resetpassword,
+} = require('../controllers/auth');
+
 const router = express.Router();
 
-
-router.route("/register").post(register);
-router.route("/login").post(login);
-router.route("/forgotpassword").post(forgotpassword);
+router.route('/register').post(register);
+router.route('/login').post(login);
+router.route('/forgotpassword').post(forgotpassword);
 // resetpassword token
-router.route("/resetpassword/:resetToken").put(resetpassword);
-
+router.route('/resetpassword/:resetToken').put(resetpassword);
 
 module.exports = router;
