c4754ff1722527dd465b0fdd93b8c11adc9a569c
MeloGuan <meloguan123@gmail.com>
2021-05-01 20:40:00 +1200

    [NZSCA-010] update email html content
    Issue: improve email content
    
    Solution: change css / add logo
    
    Risk: None
    
    Reviewed by: Garfield, Tony, Kirsty, Zhiqing

--- routes/private.js
+++ routes/private.js
@@ -5,6 +5,6 @@ const router = express.Router();
 const { getPrivateData } = require('../controllers/private');
 const { protect } = require('../middleware/auth');
 
-router.route('/').get(protect, getPrivateData);
+router.route('/:token').get(protect, getPrivateData);
 
 module.exports = router;
