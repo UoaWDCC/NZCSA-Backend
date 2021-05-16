88c4ce6c18748d65c49003bd54be58d13befda2d
长孙羽修 <yuxiu@waosamus-MacBook-Pro.local>
2021-05-17 02:43:29 +1200

    [NZCSA-020] Identifying admin/user role.

--- models/User.js
+++ models/User.js
@@ -4,6 +4,12 @@ const cryto = require('crypto');
 const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
+
+  isAdmin: {
+		type: Boolean,
+		default: false
+	},
+
   firstname: {
     type: String,
     maxlength: 20,
@@ -36,6 +42,7 @@ const UserSchema = new mongoose.Schema({
   },
   gender:{
     type: String,
+    enum: ["F", "M"],
     maxlength: 1,
   },
   university:{
@@ -51,7 +58,7 @@ const UserSchema = new mongoose.Schema({
     type: String,
   },
   dateofbirth:{
-    type: String,
+    type: Date,
   },
   wechatid:{
     type: String,
