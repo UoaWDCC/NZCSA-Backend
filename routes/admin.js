c9f13c05d2c0a768048a4f8c8ee4f8473dc43667
MeloGuan <meloguan123@gmail.com>
2021-06-21 20:59:53 +1200

    [NZCSA-022] API for admin/member Issue: Admin can not promote user to member
    Solution: Created API for promoting user to member
    
    Risk: None
    
    View by: Tony, Zhiqing

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { showMemberList, promoToMember, deleteMember } = require('../controllers/admin');
 const { addEvents, deleteEvents } = require('../controllers/events');
 
 const router = express.Router();
@@ -9,6 +10,8 @@ router.route('/add-events').post(protect, addEvents);
 router.route('/delete-events').delete(protect, deleteEvents);
 
 router.route('/show-event-user-info').delete(protect, deleteEvents);
-
+router.route('/show-member-list').get(protect, showMemberList);
+router.route('/promo-to-member').post(protect, promoToMember);
+router.route('/delete-member').delete(protect, deleteMember);
 
 module.exports = router;
