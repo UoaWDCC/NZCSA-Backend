f06333d48fddbc822ab6856a0358b0f7646f1d3f
MeloGuan <meloguan123@gmail.com>
2021-07-04 13:01:59 +1200

    1

--- routes/admin.js
+++ routes/admin.js
@@ -11,8 +11,8 @@ const { protect } = require('../middleware/auth');
 router.route('/add-events').post(protect, addEvents);
 router.route('/modify-events').post(protect, modifyEvent);
 router.route('/delete-events/:eventId').delete(protect, deleteEvents);
-
 router.route('/show-event-user-info').get(protect, shwoEventUserInfo);
+
 router.route('/show-member-list').get(protect, showMemberList);
 router.route('/promo-to-member').post(protect, promoToMember);
 router.route('/delete-member/:userId').delete(protect, deleteMember);
