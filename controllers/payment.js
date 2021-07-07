5ac64101c3806b6096cb4f7cddffbfe2e2aa1b1d
Tony Cui <Tonycui02@gmail.com>
2021-07-07 23:28:41 +1200

    setup payment router and controller

---
+++ controllers/payment.js
@@ -0,0 +1,32 @@
+
+
+exports.makePayment = async(req, res) => {
+
+    const body = {
+        "user_id": "U000004218",
+        "wallet_id": "W000045674",
+        "amount": "1.00",
+        "payment_method": "alipay",
+        "return_url": "https://merchantsite.com/checkout",
+        "callback_url": "https://merchantsite.com/confirm",
+        "merchant_reference": "dsi39ej430sks03",
+        "ip": "122.122.122.1",
+        "version": "2.0",
+        "product_name": "Pinot Noir, Otago",
+        "signature": "5fdd558530ff94f19332d17775fb13f1149d13a6cbd90a2054882a3320a2b4d3"
+    }
+
+    try {
+        
+
+        res.status(200).json({
+            success: true,
+            info: "payment success",
+        })
+    } catch (error) {
+        res.status(200).json({
+            success: false,
+            info: "payment failed",
+        })
+    }
+}
