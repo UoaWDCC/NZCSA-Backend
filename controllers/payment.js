7c0a96a5dc75c952dac1236e17ac3c9b8504ec41
Tony Cui <Tonycui02@gmail.com>
2021-07-12 10:20:21 +1200

    testing notification

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 const axios = require("axios");
 const { v4: uuidv4 } = require("uuid");
 const getSignature = require("../utils/getSignature");
@@ -13,29 +14,56 @@ exports.makePayment = async (req, res, next) => {
     wallet_id: walletId,
     amount: paymentAmount,
     payment_method: paymentMethod,
-    return_url: "https://merchantsite.com/checkout",
-    callback_url: "https://merchantsite.com/confirm",
+    return_url: "http://localhost:5000",
+    callback_url: "http://localhost:5000/api/payment/confirm",
     merchant_reference: uuidv4(),
     ip: "122.122.122.1",
     version: "2.0",
     product_name: "NZCSA Membership",
   };
 
-  const hash = getSignature(body);
+  if (paymentMethod === "wechat") {
+    body.present_qr = "1";
+  }
+
+  const hash = getSignature(body, true);
   body.signature = hash;
-  // console.log(body)
+  // console.log(body);
 
   try {
     const response = await axios.post(
       "https://api.latipay.net/v2/transaction",
       body
     );
+
+    // console.log(response.data)
     if (response.data.code === 0) {
-      res.send(response.data);
+      const { nonce, host_url, signature } = response.data;
+      const validateMessage = nonce + host_url;
+      const validateHash = getSignature(validateMessage, false);
+
+      // console.log(validateHash);
+      if (signature === validateHash) {
+        const { data } = response;
+        data.redirect_url = `${host_url}/${nonce}`;
+
+        res.status(200).json({
+          success: true,
+          data: response.data,
+        });
+      } else {
+        return next(new ErrorResponse("Signature Validation failed", 500));
+      }
     } else {
       return next(new ErrorResponse(response.data.message, response.data.code));
     }
   } catch (error) {
     next(error);
   }
+};
+
+exports.paymentNotification = async (req, res, next) => {
+  const { body } = req;
+  console.log(body);
+  next(body);
 };
--- routes/payment.js
+++ routes/payment.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { makePayment } = require("../controllers/payment");
+const { makePayment, paymentNotification } = require("../controllers/payment");
 
 const router = express.Router();
 
 const { protect } = require("../middleware/auth");
 
 router.route("/make-payment").post(protect, makePayment);
+// router.route("/payment-notification").post(protect, paymentNotification);
+router.route("/confirm").post(paymentNotification);
 
 module.exports = router;
