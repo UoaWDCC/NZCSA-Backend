008984dae844f8ad536805d51c35b97a604e65df
Melo Guan <62586556+ygua699@users.noreply.github.com>
2021-07-16 16:19:24 +1200

    Update auth.js: changed localhost

--- controllers/auth.js
+++ controllers/auth.js
@@ -65,7 +65,7 @@ exports.forgotpassword = async (req, res, next) => {
 
     await user.save();
 
-    const resetURL = `http://localhost:3000/resetPassword/${resetToken}`;
+    const resetURL = `https://nzcsa-frontend-ebon.vercel.app/resetpassword/${resetToken}`;
 
     const message = `<body style="background-color: #e9ecef;">
     <div class="preheader" style="display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;">
