9722376ae8436a481afd50940b4ed8a40ca875af
Tony Cui <Tonycui02@gmail.com>
2021-10-02 16:30:38 +1300

    added comments

--- controllers/googleSheets.js
+++ controllers/googleSheets.js
@@ -2,8 +2,14 @@ const { GoogleSpreadsheet } = require("google-spreadsheet");
 
 const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
 
+/**
+ *
+ * @route   api/admin/get-google-sheet
+ * @desc    get data from google sheet, need to first set values in config.env
+ * @access  admin/private
+ */
 exports.getGoogleSheet = async (req, res, next) => {
-  const data = [];
+  const data = []; // the data to return
 
   try {
     await doc.useServiceAccountAuth({
@@ -18,11 +24,11 @@ exports.getGoogleSheet = async (req, res, next) => {
     const { headerValues, rowCount } = sheet;
 
     rows.forEach((row) => {
-      const rowItem = {};
+      const rowItem = {}; // Row in google sheet
       headerValues.forEach((headerValue) => {
-        rowItem[headerValue] = row[headerValue];
+        rowItem[headerValue] = row[headerValue]; // process each column for the current row
       });
-      data.push(rowItem);
+      data.push(rowItem); // Add row object to data array
     });
 
     res.status(200).json({
