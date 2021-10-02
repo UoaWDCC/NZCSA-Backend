ccd920e3b8ac9aa3dcffc3ede4278f30934ed51a
Tony Cui <Tonycui02@gmail.com>
2021-10-02 16:12:08 +1300

    finished basic functionality
    returns google sheet data from api

--- controllers/googleSheets.js
+++ controllers/googleSheets.js
@@ -1,34 +1,37 @@
 const { GoogleSpreadsheet } = require("google-spreadsheet");
-const { promisify } = require("util");
-const creds = require("../creds.json");
 
-const doc = new GoogleSpreadsheet(
-  "1aVwKxtqFGVGQTWu94B65oHWVGkCbshJgR49z1sHgVt8"
-);
-
-function printStudent(student) {
-  console.log(`Name: ${student["Student Name"]}`);
-  console.log(`Major: ${student.Major}`);
-  console.log(`Home State: ${student["Home State"]}`);
-  console.log(`-----------`);
-}
+const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
 
 exports.getGoogleSheet = async (req, res, next) => {
+  const data = [];
+
   try {
-    await doc.useServiceAccountAuth(creds);
-    await doc.loadInfo(); // loads document properties and worksheets
-    console.log(doc.title);
-    const sheet = doc.sheetsByIndex[0];
-    const rows = await sheet.getRows({
-      query: "Home State = NY",
+    await doc.useServiceAccountAuth({
+      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY,
     });
+    await doc.loadInfo(); // loads document properties and worksheets
+    const sheet = doc.sheetsByIndex[0];
+
+    const rows = await sheet.getRows();
+    await sheet.loadHeaderRow();
+    const { headerValues, rowCount } = sheet;
 
     rows.forEach((row) => {
-      printStudent(row);
+      const rowItem = {};
+      headerValues.forEach((headerValue) => {
+        rowItem[headerValue] = row[headerValue];
+      });
+      data.push(rowItem);
     });
 
     res.status(200).json({
       success: true,
+      docTitle: doc.title,
+      sheetTitle: sheet.title,
+      rowCount,
+      headerValues,
+      data,
     });
   } catch (e) {
     next(e);
