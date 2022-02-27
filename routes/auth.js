fdfdfa63daebba408cf7166c45ab5e3514f59a44
Melo Guan <62586556+ygua699@users.noreply.github.com>
2022-02-27 13:59:44 +1300

    added googleauth login api (#47)
    Co-authored-by: Melo Guan <melo.guan@orionhealth.com>

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,18 @@ const {
   login,
   forgotpassword,
   resetpassword,
+  checkGoogleAuth,
 } = require("../controllers/auth");
 
 const router = express.Router();
 
+// Normal Auth
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/forgotpassword").post(forgotpassword);
-// resetpassword token
 router.route("/resetpassword/:resetToken").put(resetpassword);
+
+// GoogleAuth
+router.route("/googleauth").post(checkGoogleAuth);
 
 module.exports = router;
