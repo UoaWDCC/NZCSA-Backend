77036273288d3fa3aa796453284e93b7a93e7dd1
Kirsty Gong <kirstygong23@gmail.com>
2022-04-10 12:55:35 +1200

    fix the ordering of add event
    Add to user and event then to google sheet

--- controllers/payment.js
+++ controllers/payment.js
@@ -182,14 +182,16 @@ exports.paymentNotification = async (req, res) => {
         if (!user.attendedEvents.includes(eventId)) {
           // check if already register
           user.attendedEvents.push(eventId);
+          if (!event.userList.includes(eventId)) {
+            // check if already register
+            event.userList.push(user._id);
+          }
+
+          await event.save();
+          await user.save();
           if (
-<<<<<<< HEAD
-            event.googleSheetUrl !== null ||
-            typeof event.googleSheetUrl !== "undefined"
-=======
             event.googleSheetUrl !== undefined ||
             event.googleSheetUrl !== null
->>>>>>> 7b45856... googleSheetUrl need to check to be not null
           ) {
             console.log(event.googleSheetUrl);
             console.log(
@@ -209,13 +211,7 @@ exports.paymentNotification = async (req, res) => {
             );
           }
         }
-        if (!event.userList.includes(eventId)) {
-          // check if already register
-          event.userList.push(user._id);
-        }
 
-        await event.save();
-        await user.save();
         console.log(`event ${eventId} Added.`);
       } else {
         console.log("invalid order type");
